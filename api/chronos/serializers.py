39db3fafee88fd5138401da1084e0ebf953118df
Oscar Barba <racso_181088@hotmail.com>
2022-12-25 18:20:57 -0600

    task serializer

--- api/chronos/serializers.py
+++ api/chronos/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from .models import Task
 
+from home.serializers import MemberSerializer, HomeSerializer
+
 # Serializers define the API representation.
 class TaskSerializer(serializers.ModelSerializer):
+    assigned_to = MemberSerializer()
+    completed = serializers.SerializerMethodField()
+
     class Meta:
         model = Task
-        fields = "__all__"
+        fields = [
+            "name",
+            "assigned_to",
+            "due_date",
+            "completed",
+        ]
+
+    def get_completed(self, object):
+        return object.completed_at != None
